45ae82102a0d655442ea17e8fb5f94ecdc400528
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-07 20:56:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_Inputs_ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
